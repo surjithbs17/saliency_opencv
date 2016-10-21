9b543ecf34286f84def70521c91d716cfe99f04f
Surjith Bhagavath Singh <surjith@buildingbrains.co>
2016-10-21 05:15:30 -0600

    This file is working

---
+++ build/CMakeFiles/saliencyFineGrained.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/saliencyFineGrained.dir/staticSaliencyFineGrained.cpp.o"
+  "CMakeFiles/saliencyFineGrained.dir/main.cpp.o"
+  "saliencyFineGrained.pdb"
+  "saliencyFineGrained"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/saliencyFineGrained.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
