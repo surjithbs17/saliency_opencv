5dabfecc5f5dfc0ca762fa08ef056430df60dd52
Surjith Bhagavath Singh <surjith@buildingbrains.co>
2016-11-21 04:45:10 -0700

    Added Motion Saliency and Background subtraction

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/core/utility.hpp>
 #include <opencv2/saliency.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/video/background_segm.hpp>
 #include <iostream>
 
 using namespace std;
@@ -33,13 +34,94 @@ String video;
 Ptr<Saliency> saliencyAlgorithm;
 
 
-void saliency_spec(int input_threshold)
+
+int binwang(int input_threshold)
+{
+	
+	Mat frame;
+	Mat image;
+    Mat gray_sal;
+    Mat threshold_mat;
+    Mat fgMaskMOG2;
+	Ptr<Saliency> saliencyAlgorithm = Saliency::create( "BinWangApr2014");
+	Ptr<BackgroundSubtractor> pMOG2;
+
+  	if( saliencyAlgorithm == NULL )
+	{
+	    cout << "***Error in the instantiation of the saliency algorithm...***\n";
+	    return -1;
+	}
+
+
+	cap >> frame;
+	frame.copyTo( image );
+
+	//Ptr<Size> size = Ptr<Size>( new Size( image.cols, image.rows ) );
+    saliencyAlgorithm.dynamicCast<MotionSaliencyBinWangApr2014>()->setImagesize( image.cols, image.rows );
+    saliencyAlgorithm.dynamicCast<MotionSaliencyBinWangApr2014>()->init();
+    pMOG2 = createBackgroundSubtractorMOG2();
+
+    bool paused = false;
+    for ( ;; )
+    {
+      if( !paused )
+      {
+
+        cap >> frame;
+        cvtColor( frame, frame, COLOR_BGR2GRAY );
+
+        Mat saliencyMap;
+        if( saliencyAlgorithm->computeSaliency( frame, saliencyMap ) )
+        {
+          std::cout << "current frame motion saliency done" << std::endl;
+        }
+
+        imshow( "image", frame );
+        imshow( "saliencyMap", saliencyMap);
+        pMOG2->apply(frame, fgMaskMOG2);
+
+	    saliencyMap.convertTo(gray_sal, -1, 255,0);
+	      
+	    threshold(gray_sal,threshold_mat,input_threshold,255,THRESH_BINARY);
+
+	    imshow("Threshold Value",threshold_mat);
+	    imshow("MOG",fgMaskMOG2);
+      }
+
+      char c = (char) waitKey( 2 );
+      if( c == 'q' )
+        break;
+      if( c == 'p' )
+        paused = !paused;
+
+    }
+}
+
+int saliency_spec(int input_threshold)
 {
 	  Mat binaryMap;
 	  Mat image;
 	  Mat frame;
 	  Mat threshold_mat;
+	  Mat fgMaskMOG2;
+	  Mat gray_sal;
+	  Ptr<BackgroundSubtractor> pMOG2;
+
+	  Ptr<Saliency> saliencyAlgorithm = Saliency::create( "SPECTRAL_RESIDUAL");
+
+	  if( saliencyAlgorithm == NULL )
+	  {
+	    cout << "***Error in the instantiation of the saliency algorithm...***\n";
+	    return -1;
+	  }
+
+
+
 	  cap >> frame;
+	  pMOG2 = createBackgroundSubtractorMOG2();  
+
+
+
 	  while (!frame.empty())
 	  {
 	  	
@@ -56,22 +138,15 @@ void saliency_spec(int input_threshold)
 	      imshow( "Original Image", image );
 	      imshow( "Binary Map", binaryMap );
 	      threshold( binaryMap, threshold_mat, input_threshold, 255,0);
-	      Mat gray_sal;
+	      
 	      saliencyMap.convertTo(gray_sal, -1, 255,0);
-	      //adaptiveThreshold(gray_sal, threshold_mat, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 7, 0);
-	      //double norm = input_threshold/255;
+	      pMOG2->apply(frame, fgMaskMOG2);
 	      
 	      threshold(gray_sal,threshold_mat,input_threshold,255,THRESH_BINARY);
 
-	      //cout << "M = "<< endl << " "  << threshold_mat << endl << endl;
-	      double min, max;
-		  cv::minMaxLoc(gray_sal, &min, &max);
-
-		  cout << "MAX" << max << "MIN" << min << "Thresh " << input_threshold << endl;
-	      cout<< "After Adaptive" <<endl;
-
 	      imshow("Threshold Value",threshold_mat);
-	      char c = (char) waitKey( 0 );
+	      imshow("MOG",fgMaskMOG2);
+	      char c = (char) waitKey( 2 );
 	      if( c == 'q' )
 	        break;
 	    }
@@ -90,9 +165,10 @@ int main( int argc, char** argv )
   
 
   video = argv[1];
-  String threshold_value = argv[2];
+  String algorithm = argv[2];
+  //String threshold_value = argv[3];
 
-  int input_threshold = atoi(argv[2]);
+  int input_threshold = atoi(argv[3]);
   cout << video  << " "<<input_threshold  << endl;
   //int start_frame = parser.get<int>( 2 );
   //String training_path = parser.get<String>( 3 );
@@ -119,17 +195,21 @@ int main( int argc, char** argv )
  
 
   //instantiates the specific Saliency
-  saliencyAlgorithm = Saliency::create( "SPECTRAL_RESIDUAL");
-
-  if( saliencyAlgorithm == NULL )
-  {
-    cout << "***Error in the instantiation of the saliency algorithm...***\n";
-    return -1;
-  }
+  
 
   //createTrackbar( "Min Threshold:", "Track Bar", &lowThreshold, 255, saliency_spec );  
-  saliency_spec(input_threshold);
+  if(algorithm.find( "STATIC" ) == 0)
+  {
+  	saliency_spec(input_threshold);
+  }
+  
+  else if(algorithm.find( "MOTION" ) == 0)
+  {
+  	binwang(input_threshold);
+  }
+
   
 
   return 0;
 }
+
